6dfc0c1717d67f6e15c95329ff3af95721d2faad
MrLongarms <704718@student.dusd.net>
2024-01-23 13:52:42 -0800

    Waiting

--- ScriptingProjectP5Jureidini/Assets/Scripts/ActiveObjects.cs
+++ ScriptingProjectP5Jureidini/Assets/Scripts/ActiveObjects.cs
@@ -7,7 +7,7 @@ public class ActiveObjects : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameObject.SetActive(false);
+        gameObject.SetActive(false);
     }
 
     // Update is called once per frame
