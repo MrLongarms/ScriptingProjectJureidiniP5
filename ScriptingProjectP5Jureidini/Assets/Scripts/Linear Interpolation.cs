489e4b2c439f71f04a7f5e8e5807e26fb41911a4
MrLongarms <704718@student.dusd.net>
2024-02-14 12:58:24 -0800

    Destroy

--- ScriptingProjectP5Jureidini/Assets/Scripts/Linear Interpolation.cs
+++ ScriptingProjectP5Jureidini/Assets/Scripts/Linear Interpolation.cs
@@ -12,18 +12,18 @@ public class LinearInterpolation : MonoBehaviour
         Vector3 to = new Vector3 (5f, 6f, 7f);
 
         //Here result = (4, 5, 6)
-        Vector result = Vector3.Lerp (from, to, 0.75f);
+        Vector3 result = Vector3.Lerp (from, to, 0.75f);
     }
 
     // Update is called once per frame
     void Update()
     
     {
-        //colours are represented by 4 floats representing red, blue, green and alpha.
-        //When using Lerp, these floats are interpolated.
+        /*colours are represented by 4 floats representing red, blue, green and alpha.
+        When using Lerp, these floats are interpolated.
         
         Light.intensity = Math.Lerp(light.intensity, 8f, 0.5f
     }
     //this results in change to intensity would happen per second instead of per frame.
-/*
+*/
 }
