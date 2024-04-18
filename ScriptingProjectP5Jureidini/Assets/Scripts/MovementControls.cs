3b265f87296770b67ee2d5fd77536f78c9a0a900
MrLongarms <704718@student.dusd.net>
2024-04-18 12:58:51 -0700

    Classes Done

--- ScriptingProjectP5Jureidini/Assets/Scripts/MovementControls.cs
+++ ScriptingProjectP5Jureidini/Assets/Scripts/MovementControls.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class MovementControls : MonoBehaviour
 {
-    
+    public float speed;
+    public float turnSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +16,15 @@ public class MovementControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+    }
+
+    void Movement ()
+    {
+        float forwardMovement = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float turnMovement = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
+
+        transform.Translate(Vector3.forward * forwardMovement);
+        transform.Rotate(Vector3.up * turnMovement);
     }
 }
